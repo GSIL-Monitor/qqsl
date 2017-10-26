b549cae2e9a82fe55a7075953c98452f40b1ee7f
雪庭 <119238122@qq.com>
2017-10-26 10:07:53 +0800

    project:IconType

--- src/main/java/com/hysw/qqsl/cloud/core/entity/data/Project.java
+++ src/main/java/com/hysw/qqsl/cloud/core/entity/data/Project.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.*;
+import javax.swing.*;
 
 import com.hysw.qqsl.cloud.CommonAttributes;
 import org.codehaus.jackson.annotate.JsonIgnore;
@@ -12,217 +13,303 @@ import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 
 /**
- *  项目实体类
- * @author leinuo  
+ * 项目实体类
  *
- * @date  2016年1月12日
+ * @author leinuo
+ * @date 2016年1月12日
  */
 @Entity
 @Table(name = "project")
-@SequenceGenerator(name = "sequenceGenerator",sequenceName = "project_sequence")
-@JsonIgnoreProperties(value={"hibernateLazyInitializer"})
-public class Project extends BaseEntity{
-	private static final long serialVersionUID = 4471107521461776780L;
-	/** 阿里云路径 */
-	private String treePath;
-	/** 项目编号 */
-	private String code;
-	/** 项目名称 */
-	private String name;
-	/** 项目类型*/
-	private Type type;
-	/** 建设区域 */
-	private String buildArea;
-	/** 用户 */
-	private User user;
-	/** 开工时间 */
-	private Date startDate;
-	/** 竣工时间 */
-	private Date endDate;
-	/** 信息字符串 */
-	private String infoStr;
-	/** 规划 */
-	private Long planning;
-	/**项目相关的要素*/
+@SequenceGenerator(name = "sequenceGenerator", sequenceName = "project_sequence")
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer"})
+public class Project extends BaseEntity {
+    private static final long serialVersionUID = 4471107521461776780L;
+    /**
+     * 阿里云路径
+     */
+    private String treePath;
+    /**
+     * 项目编号
+     */
+    private String code;
+    /**
+     * 项目名称
+     */
+    private String name;
+    /**
+     * 项目类型
+     */
+    private Type type;
+    /**
+     * 建设区域
+     */
+    private String buildArea;
+    /**
+     * 用户
+     */
+    private User user;
+    /**
+     * 开工时间
+     */
+    private Date startDate;
+    /**
+     * 竣工时间
+     */
+    private Date endDate;
+    /**
+     * 信息字符串
+     */
+    private String infoStr;
+    /**
+     * 规划
+     */
+    private Long planning;
+    /**
+     * 项目相关的要素
+     */
     private List<ElementDB> elementDBs = new ArrayList<ElementDB>();
-	/** 坐标对象,当长度为1表示线或面坐标，不唯一表示点坐标*/
-	private List<Coordinate> coordinates = new ArrayList<Coordinate>();
-	private String logStr;
-	/** 占用空间 */
-	private long curSpaceNum;
-	/**
-	 * 建筑物
-	 */
-	private List<Build> builds = new ArrayList<>();
-	///////////////////////////
-	/**
-	 * 企业间查看
-	 */
-	private String shares;
-	/**
-	 * 子账号间协同
-	 */
-	private String cooperate;
-	/**
-	 * 子账号查看企业
-	 */
-	private String views;
+    /**
+     * 坐标对象,当长度为1表示线或面坐标，不唯一表示点坐标
+     */
+    private List<Coordinate> coordinates = new ArrayList<Coordinate>();
+    private String logStr;
+    /**
+     * 占用空间
+     */
+    private long curSpaceNum;
+    /**
+     * 建筑物
+     */
+    private List<Build> builds = new ArrayList<>();
+    ///////////////////////////
+    /**
+     * 企业间查看
+     */
+    private String shares;
+    /**
+     * 子账号间协同
+     */
+    private String cooperate;
+    /**
+     * 子账号查看企业
+     */
+    private String views;
 
-	/**项目类型*/
-	public enum Type {
-		/**人畜饮水工程 */
-		DRINGING_WATER,
-		/** 灌溉工程 */
-		AGRICULTURAL_IRRIGATION,
-		/** 防洪减灾工程 */
-		FLOOD_DEFENCES,
-		/** 水土保持工程 */
-		CONSERVATION,
-		/** 农村小水电工程 */
-		HYDROPOWER_ENGINEERING,
-		/** 供水保障工程 */
-		WATER_SUPPLY
-	}
+    /**
+     * 项目图标类型
+     */
+    private IconType iconType;
 
-	public Project() {
-	}
+    /**
+     * 项目类型
+     */
+    public enum Type {
+        /**
+         * 人畜饮水工程
+         */
+        DRINGING_WATER,
+        /**
+         * 灌溉工程
+         */
+        AGRICULTURAL_IRRIGATION,
+        /**
+         * 防洪减灾工程
+         */
+        FLOOD_DEFENCES,
+        /**
+         * 水土保持工程
+         */
+        CONSERVATION,
+        /**
+         * 农村小水电工程
+         */
+        HYDROPOWER_ENGINEERING,
+        /**
+         * 供水保障工程
+         */
+        WATER_SUPPLY
+    }
 
-	public String getTreePath() {
-		return treePath;
-	}
-	public void setTreePath(String treePath) {
-		this.treePath = treePath;
-	}
-	public String getCode() {
-		return code;
-	}
-	public void setCode(String code) {
-		this.code = code;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Type getType() {
-		return type;
-	}
-	public void setType(Type type) {
-		this.type = type;
-	}
-	public String getBuildArea() {
-		return buildArea;
-	}
-	public void setBuildArea(String buildArea) {
-		this.buildArea = buildArea;
-	}
+    /**
+     * 项目图标类型
+     */
+    public enum IconType {
+        STYLE_0,
+        STYLE_1,
+        STYLE_2,
+        STYLE_3,
+        STYLE_4,
+        STYLE_5
+    }
 
-	@ManyToOne(fetch=FetchType.EAGER)
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	public Date getStartDate() {
-		return startDate;
-	}
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-	public Date getEndDate() {
-		return endDate;
-	}
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-	@JsonIgnore
-	public String getInfoStr() {
-		return infoStr;
-	}
-	public void setInfoStr(String infoStr) {
-		this.infoStr = infoStr;
-	}
-	public Long getPlanning() {
-		return planning;
-	}
+    public Project() {
+    }
 
-	public void setPlanning(Long planning) {
-		this.planning = planning;
-	}
+    public String getTreePath() {
+        return treePath;
+    }
 
-	@OneToMany(cascade={CascadeType.PERSIST,CascadeType.REMOVE},  fetch=FetchType.LAZY, mappedBy="project")	
-	@JsonIgnore
-	public List<ElementDB> getElementDBs() {
-		return elementDBs;
-	}
-	public void setElementDBs(List<ElementDB> elementDBs) {
-		this.elementDBs = elementDBs;
-	}
+    public void setTreePath(String treePath) {
+        this.treePath = treePath;
+    }
 
-	@OneToMany(cascade={CascadeType.PERSIST,CascadeType.REMOVE},  fetch=FetchType.LAZY, mappedBy="project")
-	@JsonIgnore
-	public List<Coordinate> getCoordinates() {
-		return coordinates;
-	}
-	public void setCoordinates(List<Coordinate> coordinates) {
-		this.coordinates = coordinates;
-	}
-	@Transient
-	public String getLogStr() {
-		return logStr;
-	}
+    public String getCode() {
+        return code;
+    }
 
-	public void setLogStr(String logStr) {
-		this.logStr = logStr;
-	}
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-	@OneToMany(cascade={CascadeType.PERSIST,CascadeType.REMOVE},  fetch=FetchType.LAZY, mappedBy="project")
-	@JsonIgnore
-	public List<Build> getBuilds() {
-		return builds;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setBuilds(List<Build> builds) {
-		this.builds = builds;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	////////////////
-	@Basic(fetch = FetchType.EAGER)
-	@Column(columnDefinition = "text")
-	public String getShares() {
-		return shares;
-	}
+    public Type getType() {
+        return type;
+    }
 
-	public void setShares(String shares) {
-		this.shares = shares;
-	}
-	@Basic(fetch = FetchType.EAGER)
-	@Column(columnDefinition = "text")
-	public String getCooperate() {
-		return cooperate;
-	}
+    public void setType(Type type) {
+        this.type = type;
+    }
 
-	public void setCooperate(String cooperate) {
-		this.cooperate = cooperate;
-	}
+    public String getBuildArea() {
+        return buildArea;
+    }
 
-	@Basic(fetch = FetchType.EAGER)
-	@Column(columnDefinition = "text")
-	public String getViews() {
-		return views;
-	}
+    public void setBuildArea(String buildArea) {
+        this.buildArea = buildArea;
+    }
 
-	public void setViews(String views) {
-		this.views = views;
-	}
+    @ManyToOne(fetch = FetchType.EAGER)
+    public User getUser() {
+        return user;
+    }
 
-	public long getCurSpaceNum() {
-		return curSpaceNum;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-	public void setCurSpaceNum(long curSpaceNum) {
-		this.curSpaceNum = curSpaceNum;
-	}
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    @JsonIgnore
+    public String getInfoStr() {
+        return infoStr;
+    }
+
+    public void setInfoStr(String infoStr) {
+        this.infoStr = infoStr;
+    }
+
+    public Long getPlanning() {
+        return planning;
+    }
+
+    public void setPlanning(Long planning) {
+        this.planning = planning;
+    }
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.LAZY, mappedBy = "project")
+    @JsonIgnore
+    public List<ElementDB> getElementDBs() {
+        return elementDBs;
+    }
+
+    public void setElementDBs(List<ElementDB> elementDBs) {
+        this.elementDBs = elementDBs;
+    }
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.LAZY, mappedBy = "project")
+    @JsonIgnore
+    public List<Coordinate> getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(List<Coordinate> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    @Transient
+    public String getLogStr() {
+        return logStr;
+    }
+
+    public void setLogStr(String logStr) {
+        this.logStr = logStr;
+    }
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.LAZY, mappedBy = "project")
+    @JsonIgnore
+    public List<Build> getBuilds() {
+        return builds;
+    }
+
+    public void setBuilds(List<Build> builds) {
+        this.builds = builds;
+    }
+
+    ////////////////
+    @Basic(fetch = FetchType.EAGER)
+    @Column(columnDefinition = "text")
+    public String getShares() {
+        return shares;
+    }
+
+    public void setShares(String shares) {
+        this.shares = shares;
+    }
+
+    @Basic(fetch = FetchType.EAGER)
+    @Column(columnDefinition = "text")
+    public String getCooperate() {
+        return cooperate;
+    }
+
+    public void setCooperate(String cooperate) {
+        this.cooperate = cooperate;
+    }
+
+    @Basic(fetch = FetchType.EAGER)
+    @Column(columnDefinition = "text")
+    public String getViews() {
+        return views;
+    }
+
+    public void setViews(String views) {
+        this.views = views;
+    }
+
+    public long getCurSpaceNum() {
+        return curSpaceNum;
+    }
+
+    public void setCurSpaceNum(long curSpaceNum) {
+        this.curSpaceNum = curSpaceNum;
+    }
+
+    public IconType getIconType() {
+        return iconType;
+    }
+
+    public void setIconType(IconType iconType) {
+        this.iconType = iconType;
+    }
 }
