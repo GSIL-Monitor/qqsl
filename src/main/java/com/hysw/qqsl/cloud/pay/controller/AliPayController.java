0c7332b97de5602a772e0995938bf0ff6a76e8ec
liujb <1321404703@qq.com>
2017-11-07 18:34:17 +0800

    支付宝回调

--- src/main/java/com/hysw/qqsl/cloud/pay/controller/AliPayController.java
+++ src/main/java/com/hysw/qqsl/cloud/pay/controller/AliPayController.java
@@ -14,6 +14,8 @@ import com.hysw.qqsl.cloud.pay.service.TradeService;
 import com.hysw.qqsl.cloud.pay.service.TurnoverService;
 import com.hysw.qqsl.cloud.pay.service.aliPay.AliPayService;
 import net.sf.json.JSONObject;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresRoles;
@@ -48,6 +50,7 @@ public class AliPayController {
     private TradeService tradeService;
     @Autowired
     private TurnoverService turnoverService;
+    Log logger = LogFactory.getLog(this.getClass());
 
     //private static final String RETURN_URL = "http://4107ce0a.all123.net/qqsl.web/tpls/productModule/paySuccess.html";
     private static final String RETURN_URL = "http://112.124.104.190/tpls/productModule/aliPaySuccess.html";
@@ -105,6 +108,7 @@ public class AliPayController {
     public
     @ResponseBody
     String notify(HttpServletRequest request) throws ServletException, IOException, AlipayApiException {
+        logger.info("支付成功,进入回调");
         Map<String, String> params = new HashMap<>();
         Map requestParams = request.getParameterMap();
         for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext(); ) {
@@ -124,11 +128,11 @@ public class AliPayController {
         boolean signVerified = AlipaySignature.rsaCheckV1(params, CommonAttributes.ALIPAY_PUBLIC_KEY, CommonAttributes.CHARSET, CommonAttributes.SIGN_TYPE); //调用SDK验证签名
         if (signVerified) {
             if (tradeStatus.equals("TRADE_FINISHED") || tradeStatus.equals("TRADE_SUCCESS")) {
+                logger.info("回调参数验证成功");
                 //判断该笔订单是否在商户网站中已经做过处理
                 //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
                 //请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
                 //如果有做过处理，不执行商户的业务程序
-
                 //注意：
                 //如果签约的是可退款协议，退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
                 //如果没有签约可退款协议，那么付款完成后，支付宝系统发送该交易状态通知。
@@ -158,8 +162,10 @@ public class AliPayController {
 //            return "success";    //请不要修改或删除
             //////////////////////////////////////////////////////////////////////////////////////////
         } else {//验证失败
+            logger.info("回调参数验证失败");
             return "failure";
         }
+        logger.info("回调失败");
         return "failure";
     }
 
