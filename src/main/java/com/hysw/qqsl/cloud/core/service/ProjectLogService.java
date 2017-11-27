8349b0f42c9607697565813e7a46a4bc68780792
hysw-chenl <84781320@qq.com>
2017-11-27 15:01:40 +0800

    增加日志

--- src/main/java/com/hysw/qqsl/cloud/core/service/ProjectLogService.java
+++ src/main/java/com/hysw/qqsl/cloud/core/service/ProjectLogService.java
@@ -1,4 +1,17 @@
 package com.hysw.qqsl.cloud.core.service;
 
-public class ProjectLogService {
+import com.hysw.qqsl.cloud.core.dao.ProjectLogDao;
+import com.hysw.qqsl.cloud.core.entity.data.ProjectLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("projectLogService")
+public class ProjectLogService extends BaseService<ProjectLog, Long> {
+    @Autowired
+    private ProjectLogDao projectLogDao;
+    @Autowired
+    public void setBaseDao(ProjectLogDao projectLogDao) {
+        super.setBaseDao(projectLogDao);
+    }
+
 }
