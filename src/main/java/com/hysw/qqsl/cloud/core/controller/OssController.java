2ab53a97f4c61a321896b97356fb78b901073a78
hysw-chenl <84781320@qq.com>
2018-04-10 10:59:52 +0800

    增加oss/directToken接口及方法 增加panorama/add接口及方法

--- src/main/java/com/hysw/qqsl/cloud/core/controller/OssController.java
+++ src/main/java/com/hysw/qqsl/cloud/core/controller/OssController.java
@@ -290,6 +290,5 @@ public class OssController {
 			response.getWriter().println(callbackFunName + "( "+results+" )");
 		response.setStatus(HttpServletResponse.SC_OK);
 		response.flushBuffer();
-
 	}
 }
