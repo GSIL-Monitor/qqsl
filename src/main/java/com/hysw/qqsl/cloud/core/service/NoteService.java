08c4e2878f806a5e1892baf85c85eed9d068ea36
liujb <1321404703@qq.com>
2018-04-25 15:30:16 +0800

    note  panorama

--- src/main/java/com/hysw/qqsl/cloud/core/service/NoteService.java
+++ src/main/java/com/hysw/qqsl/cloud/core/service/NoteService.java
@@ -444,7 +444,7 @@ public class NoteService extends BaseService<Note, Long> {
 		//必填:短信模板-可在短信控制台中找到
 		request.setTemplateCode("SMS_132991461");
 		//可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-		request.setTemplateParam("{\"company\":"+companyName+"}");
+		request.setTemplateParam("{\"company\":\""+companyName+"\"}");
 		//选填-上行短信扩展码(无特殊需求用户请忽略此字段)
 		//request.setSmsUpExtendCode("90997");
 		//可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
---
+++ src/test/java/com/hysw/qqsl/cloud/core/service/NoteServiceTest.java
@@ -0,0 +1,26 @@
+package com.hysw.qqsl.cloud.core.service;
+
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.hysw.qqsl.cloud.BaseTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Create by leinuo on 18-4-25 上午10:05
+ * <p>
+ * qq:1321404703 https://github.com/leinuo2016
+ */
+public class NoteServiceTest extends BaseTest{
+
+    @Autowired
+    private NoteService noteService;
+
+
+
+    @Test
+    public void sendSmsTest() throws Exception{
+        SendSmsResponse sendSmsResponse =    noteService.sendSms("18661925010","qqsl");
+    }
+
+
+}
