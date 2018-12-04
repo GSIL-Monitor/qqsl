d9fc743600199e2cdb62dbbece84633ae08b2c4b
hysw-chenl <84781320@qq.com>
2018-12-04 15:13:22 +0800

    调整结构

--- src/main/java/com/hysw/qqsl/cloud/core/controller/ShapeController.java
+++ src/main/java/com/hysw/qqsl/cloud/core/controller/ShapeController.java
@@ -885,47 +885,12 @@ public class ShapeController {
         if (build == null) {
             return MessageService.message(Message.Type.DATA_NOEXIST);
         }
-        if (buildAttributes != null) {
-            BuildAttribute buildAttribute1 = null;
-            for (Object buildAttribute : JSONArray.fromObject(buildAttributes)) {
-                JSONObject jsonObject1 = JSONObject.fromObject(buildAttribute);
-                if (jsonObject1.get("id") != null) {
-                    buildAttribute1 = buildAttributeService.find(Long.valueOf(jsonObject1.get("id").toString()));
-                    if (!buildAttribute1.getBuild().getId().equals(build.getId())) {
-                        return MessageService.message(Message.Type.DATA_REFUSE);
-                    }
-                    buildAttribute1.setValue(jsonObject1.get("value").toString());
-                } else if (jsonObject1.get("alias").equals("position") || jsonObject1.get("alias").equals("designElevation") || jsonObject1.get("alias").equals("remark")|| jsonObject1.get("alias").equals("center")) {
-                    JSONObject jsonObject;
-                    if (jsonObject1.get("alias").equals("center")) {
-                        continue;
-                    }
-                    if (jsonObject1.get("alias").equals("position") && jsonObject1.get("value") != null) {
-                        String[] centers = jsonObject1.get("value").toString().split(",");
-                        if (centers.length != 2) {
-                            return MessageService.message(Message.Type.FAIL);
-                        }
-                        jsonObject = new JSONObject();
-                        jsonObject.put("lon", centers[0]);
-                        jsonObject.put("lat", centers[1]);
-                        build.setPositionCoor(jsonObject.toString());
-                    }
-                    if (jsonObject1.get("alias").equals("designElevation") && jsonObject1.get("value") != null) {
-                        build.setDesignElevation(jsonObject1.get("value").toString());
-                    }
-                    if (jsonObject1.get("alias").equals("remark") && jsonObject1.get("value") != null) {
-                        build.setRemark(jsonObject1.get("value").toString());
-                    }
-                } else {
-                    buildAttribute1 = new BuildAttribute();
-                    buildAttribute1.setValue(jsonObject1.get("value").toString());
-                    buildAttribute1.setAlias(jsonObject1.get("alias").toString());
-                    buildAttribute1.setBuild(build);
-                }
-                if (buildAttribute1 != null) {
-                    buildAttributeService.save(buildAttribute1);
-                }
-            }
+        String s = buildAttributeService.editBuildAttribute(build, buildAttributes);
+        if (s == null) {
+            return MessageService.message(Message.Type.DATA_REFUSE);
+        }
+        if (s.equals("")) {
+            return MessageService.message(Message.Type.FAIL);
         }
         buildService.save(build);
         return MessageService.message(Message.Type.OK);
--- src/main/java/com/hysw/qqsl/cloud/core/service/BuildAttributeService.java
+++ src/main/java/com/hysw/qqsl/cloud/core/service/BuildAttributeService.java
@@ -4,6 +4,8 @@ import com.hysw.qqsl.cloud.core.dao.BuildAttributeDao;
 import com.hysw.qqsl.cloud.core.entity.Filter;
 import com.hysw.qqsl.cloud.core.entity.data.Build;
 import com.hysw.qqsl.cloud.core.entity.data.BuildAttribute;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,5 +34,51 @@ public class BuildAttributeService extends BaseService<BuildAttribute,Long> {
         List<Filter> filters = new ArrayList<>();
         filters.add(Filter.eq("build", build));
         return buildAttributeDao.findList(0, null, filters);
+    }
+
+    public String editBuildAttribute(Build build, Object buildAttributes) {
+        if (buildAttributes != null) {
+            BuildAttribute buildAttribute1 = null;
+            for (Object buildAttribute : JSONArray.fromObject(buildAttributes)) {
+                JSONObject jsonObject1 = JSONObject.fromObject(buildAttribute);
+                if (jsonObject1.get("id") != null) {
+                    buildAttribute1 = find(Long.valueOf(jsonObject1.get("id").toString()));
+                    if (!buildAttribute1.getBuild().getId().equals(build.getId())) {
+                        return null;
+                    }
+                    buildAttribute1.setValue(jsonObject1.get("value").toString());
+                } else if (jsonObject1.get("alias").equals("position") || jsonObject1.get("alias").equals("designElevation") || jsonObject1.get("alias").equals("remark")|| jsonObject1.get("alias").equals("center")) {
+                    JSONObject jsonObject;
+                    if (jsonObject1.get("alias").equals("center")) {
+                        continue;
+                    }
+                    if (jsonObject1.get("alias").equals("position") && jsonObject1.get("value") != null) {
+                        String[] centers = jsonObject1.get("value").toString().split(",");
+                        if (centers.length != 2) {
+                            return "";
+                        }
+                        jsonObject = new JSONObject();
+                        jsonObject.put("lon", centers[0]);
+                        jsonObject.put("lat", centers[1]);
+                        build.setPositionCoor(jsonObject.toString());
+                    }
+                    if (jsonObject1.get("alias").equals("designElevation") && jsonObject1.get("value") != null) {
+                        build.setDesignElevation(jsonObject1.get("value").toString());
+                    }
+                    if (jsonObject1.get("alias").equals("remark") && jsonObject1.get("value") != null) {
+                        build.setRemark(jsonObject1.get("value").toString());
+                    }
+                } else {
+                    buildAttribute1 = new BuildAttribute();
+                    buildAttribute1.setValue(jsonObject1.get("value").toString());
+                    buildAttribute1.setAlias(jsonObject1.get("alias").toString());
+                    buildAttribute1.setBuild(build);
+                }
+                if (buildAttribute1 != null) {
+                    save(buildAttribute1);
+                }
+            }
+        }
+        return "1";
     }
 }
